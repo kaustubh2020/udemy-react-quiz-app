02ae9893850e562f6dab07c43c32bdb0bcb3e1e4
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2023-12-05 18:09:20 +0530

    Quiz completed compenent logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,45 @@
 import { useState } from "react";
+import quizCompleteImg from "../assets/quiz-complete.png";
+
+import QUESTIONS from "../questions";
 
 const Quiz = () => {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState([]);
-  const [userAnswers, setUserAnswers] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active quetsion</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy Icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const suffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  suffledAnswers.sort(() => Math.random() - 0.5);
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevAnswers) => {
+      return [...prevAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {suffledAnswers.map((answer) => (
+          <li key={answer} className="answer">
+            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Quiz;
